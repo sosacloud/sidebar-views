d624381491c975902df1a6bee2c08ed885a28c6f
deadcode <josephsonsjoseph@gmail.com>
2019-07-12 11:46:08 -0700

    "Fixed syntax typo in db conneciton object"

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   host: `localhost`,
-  username: 'root',
+  user: 'root',
   password: 's1c4o8t7t',
   database: 'relatedTracks'
 });
